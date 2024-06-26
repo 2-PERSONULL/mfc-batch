47117100bceec100b4e1301cc6ba02ad9905b1fe
pockyjx <0127828b@gmail.com>
2024-06-26 12:06:54 +0900

    feat: 파트너 집계 배치 구현

--- src/main/java/com/mfc/batch/batch/infrastructure/PartnerSummaryRepository.java
+++ src/main/java/com/mfc/batch/batch/infrastructure/PartnerSummaryRepository.java
@@ -1,8 +1,12 @@
 package com.mfc.batch.batch.infrastructure;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mfc.batch.batch.domain.PartnerSummary;
+import com.mfc.batch.batch.domain.PostSummary;
 
 public interface PartnerSummaryRepository extends JpaRepository<PartnerSummary, Long> {
+	Optional<PartnerSummary> findByPartnerId(String partnerId);
 }
